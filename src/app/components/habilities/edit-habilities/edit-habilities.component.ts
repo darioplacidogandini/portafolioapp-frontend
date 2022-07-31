2da6bacfb750838694bb52ed976e13b1170bbc05
darioplacidogandini <darioplacidogandini@outlook.com>
2022-07-31 14:14:01 -0300

    .

--- src/app/components/habilities/edit-habilities/edit-habilities.component.ts
+++ src/app/components/habilities/edit-habilities/edit-habilities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Hability } from 'src/app/model/hability.model';
 import { HabilitiesService } from 'src/app/services/habilities.service';
 
@@ -9,7 +10,7 @@ import { HabilitiesService } from 'src/app/services/habilities.service';
 })
 export class EditHabilitiesComponent implements OnInit {
 
-  constructor(private habilitiesService:HabilitiesService) {}
+  constructor(private habilitiesService:HabilitiesService,private route:Router) {}
 
   id:number = 0;
   habilities:Hability = new Hability();
@@ -26,6 +27,7 @@ export class EditHabilitiesComponent implements OnInit {
       console.log(data);
       this.habilitiesService.list();
     },error => console.log(error));
+    this.route.navigate(['']);
     window.location.reload();
   }
 }
