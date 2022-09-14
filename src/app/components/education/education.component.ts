f399d8ffe468df273a91271a00e6767f2189e93a
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-14 01:01:36 -0300

    .

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogsService } from 'src/app/services/dialogs.service';
-import { Education } from 'src/app/model/education.model';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { EducationService } from 'src/app/services/education.service';
-import { AddEducationComponent } from './add-education/add-education.component';
-import { EditEducationComponent } from './edit-education/edit-education.component';
+import education from '../../assets/education.json'
+
+interface Education {
+  id:number;
+  title:string;
+  institution:string;
+  date:string;
+  duration:number;
+  institutionLogo:string;  
+}
 
 @Component({
   selector: 'app-education',
@@ -14,41 +17,10 @@ import { EditEducationComponent } from './edit-education/edit-education.componen
 })
 export class EducationComponent implements OnInit {
 
-  education:Education[] = [];
-  id:number = 0;
+  education: Education[] = education;
 
-  constructor(private dialogsService:DialogsService,private educationService:EducationService,
-    private authService:AuthenticationService,public dialog:MatDialog) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.list();
-  }
-
-  public isUserLoggedIn() {
-    return this.authService.isUserLoggedIn();
-  }
-
-  public list() {
-    this.educationService.list().subscribe(data => {
-      this.education = data;
-    });
-  }
-
-  public openAddDialog() {
-    this.dialog.open(AddEducationComponent,this.dialogsService.dialogConfig);
-  }
-
-  public openEditDialog(id:number) {
-    this.educationService.id = id;
-    this.dialog.open(EditEducationComponent,this.dialogsService.dialogConfig);
-  }
-
-  public delete(id:number) {
-    this.educationService.delete(id).subscribe(data => {
-      console.log(data);
-      this.list();
-    });
-  }
-
+  ngOnInit(): void {}
 }
 
